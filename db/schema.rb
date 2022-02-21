b1d1acfee7ae2631a46308a30e5fcce26e4e2bac
anniegomezz <anniegomez@my.unt.edu>
2022-02-21 13:29:42 -0600

    added migration for image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_17_030112) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_21_192906) do
   create_table "ingredients", force: :cascade do |t|
     t.text "content"
     t.integer "recipe_id", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_17_030112) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "steps", force: :cascade do |t|
